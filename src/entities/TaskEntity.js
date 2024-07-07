3e913c47a15e5eb05f3c98aaf8c5f058a15f50a9
ArtthSilva <arthur12pereira1@gmail.com>
2024-07-07 15:12:12 -0300

    initial commit

---
+++ src/entities/TaskEntity.js
@@ -0,0 +1,7 @@
+export class TaskEntity {
+    constructor({ id, title, completed }) {
+        this.id = id
+        this.title = title
+        this.completed = completed
+    }
+}
---
+++ src/ui/pages/HomePage.js
@@ -0,0 +1,8 @@
+
+import  styles  from "./HomePage.module.css";
+export const HomePage = () => {
+    return (
+        <h1>To Do List</h1>
+    );
+}
+
